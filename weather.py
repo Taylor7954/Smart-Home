e85e7b33825886c64431b240d62ddce12048e25a
Pyroclast256 <37544659+Pyroclast256@users.noreply.github.com>
2018-03-30 11:17:49 -0500

    Weather API

---
+++ weather.py
@@ -0,0 +1,20 @@
+import requests
+import forecastio
+import time
+
+from pprint import pprint
+
+def main():
+    lat = 33.699657
+    lon = -86.635033
+    api_key = "83942510644cc6a8bfceae5bed2e6ed8"
+    api_call = f"https://api.darksky.net/forecast/{api_key}/{lat},{lon},{int(time.time())}?exclude=flags,alerts,currently,daily,minutely"
+    r = requests.get(api_call)
+    print(r.status_code)
+    print(r.text)
+    forecast = r.json()
+    pprint(forecast)
+
+
+if __name__ == '__main__': 
+    main()
