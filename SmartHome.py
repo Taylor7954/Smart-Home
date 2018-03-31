317b50622da4fdcc4032914fc071435ca52b1f2f
Taylor7954 <31739759+Taylor7954@users.noreply.github.com>
2018-03-31 14:59:32 -0500

    More bug fixes to register

--- SmartHome.py
+++ SmartHome.py
@@ -28,7 +28,6 @@ def contact():
 
 class RegisterForm(Form):
 	name = StringField('Name', [validators.Length(min=1, max=50)])
-	#username = StringField('Username', [validators.Length(min=4, max=25)])
 	email = StringField('Email', [validators.Length(min=6, max= 50)])
 	address = StringField('Address', [validators.Length(min=6, max= 50)])
 	city = StringField('City', [validators.Length(min=6, max= 50)])
@@ -87,9 +86,6 @@ def register():
 	
 		#save changes
 		session.commit()
-	
-		#close session
-		session.close()
 		
 		return render_template('register.html')
 	return render_template('register.html', form=form)
