91c78ab320d20fba35e2ec278b3b3ffc13f56fb0
William Setzer <wjsetzer@uab.edu>
2018-03-31 17:10:03 -0500

    Added goslate to dependencies
    Signed-off-by: William Setzer <wjsetzer@uab.edu>

--- Translation.py
+++ Translation.py
@@ -1,13 +1,13 @@
 import goslate
 
 def main():
-   with open("insertHere", 'r') as f:
-       xml_to_trans = f.read()
+    with open("insertHere", 'r') as f:
+        xml_to_trans = f.read()
     gs = goslate.Goslate()
     gs.translate(xml_to_trans)
 
     if __name__ == '__main__': 
-    main()
+        main()
 
     ##This code needs to have goslate installed in order to work
     ##To install use cmd pip install goslate
